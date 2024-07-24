25c89e35b4a8900b899a6fefa83390f6aa828344
Lecio Ribeiro <139591377+leciojor@users.noreply.github.com>
2024-07-23 21:34:10 -0600

    more updates to about us

--- src/components/About.js
+++ src/components/About.js
@@ -4,19 +4,25 @@ const About = () => {
     return (
         <section id="Sobre nós" className='about'>
             <div className="main-text">
-                This is the main big text
+                Alugando impressoras desde 2020...
             </div>
-            <div className="rectangular-components">
-                <div className="rectangle">
-                    <p>Sample text 1</p>
+            <div className='bottom-container'>
+                <div className="about-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
                 </div>
-                <div className="rectangle">
-                    <p>Sample text 2</p>
-                </div>
-                <div className="rectangle">
-                    <p>Sample text 3</p>
+                <div className="rectangular-components">
+                    <div className="rectangle">
+                        <p>Sample text 1</p>
+                    </div>
+                    <div className="rectangle">
+                        <p>Sample text 2</p>
+                    </div>
+                    <div className="rectangle">
+                        <p>Sample text 3</p>
+                    </div>
                 </div>
             </div>
+
         </section>
     );
 };
