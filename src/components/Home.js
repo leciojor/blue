0a0fd9ffa70bf41ac7c4cd08235e73f3ab1cd967
= <leciojoaoorsiribeiro@gmail.com>
2024-07-27 13:48:22 -0700

    .

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,8 @@ const Home = () => {
     return (
         <section id="home" className='home'>
             <div className="center-content">
-                <img src='' alt="Main Title" className="main-title" />
+                <img src="https://raw.githubusercontent.com/leciojor/blue/1ff4a3e279b979586af3e7da2862cf3a9d2907ba/src/components/imgs/logo/Blue%20INK%20(1).svg" 
+                    alt="Main Title" className="main-title" />
                 <button 
                     className="redirect-button" 
                     onClick={() => window.location.href = 'http://example.com'}
